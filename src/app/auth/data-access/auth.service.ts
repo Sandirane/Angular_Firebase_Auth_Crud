5785321c225cf3cd6c4da4b278ee7ee87aa1109f
Sandirane <kumarasamy.sandirane.77@gmail.com>
2024-09-29 15:50:47 +0200

    initial config and authentification

---
+++ src/app/auth/data-access/auth.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from '@angular/fire/auth';
+
+export interface User {
+  email: string;
+  password: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private auth: Auth) { }
+
+  signUp(user: User) {
+    return createUserWithEmailAndPassword(
+      this.auth,
+      user.email,
+      user.password
+    );
+  }
+
+  signIn(user: User) {
+    return signInWithEmailAndPassword(
+      this.auth,
+      user.email,
+      user.password
+    );
+  }
+
+}
---
+++ src/app/auth/features/sign-in/sign-in.component.ts
@@ -0,0 +1,57 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '@app/auth/data-access/auth.service';
+import { isRequired, hasEmailError } from '@app/auth/utils/validators';
+
+interface FormSignIn {
+  email: FormControl<string | null>,
+  password: FormControl<string | null>
+}
+
+@Component({
+  selector: 'app-sign-in',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule, RouterLink],
+  templateUrl: './sign-in.component.html',
+  styleUrl: './sign-in.component.css'
+})
+export default class SignInComponent {
+
+  constructor(
+    private formBuiler: FormBuilder,
+    private authService: AuthService,
+    private router: Router) { }
+
+  isRequired(fiels: 'email' | 'password') {
+    return isRequired(fiels, this.form);
+  }
+
+  hasEmailError() {
+    return hasEmailError(this.form)
+  }
+
+  form = this.formBuiler.group<FormSignIn>({
+    email: this.formBuiler.control('', [
+      Validators.required,
+      Validators.email,
+    ]),
+    password: this.formBuiler.control('', Validators.required),
+  });
+
+  async submitSignIn() {
+    if (this.form.invalid) return;
+
+    try {
+      const { email, password } = this.form.value
+      if (!email || !password) return;
+      await this.authService.signIn({ email, password })
+      alert("User is connect")
+      this.router.navigateByUrl('/tasks')
+    } catch (error) {
+      alert("User is not connect")
+    }
+  }
+
+}
